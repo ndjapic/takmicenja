483e15350a235f7dfc86ffca59be7b9e9c7b8fcb
ndjapic@gmail.com <ndjapic@gmail.com@266d48c1-7390-4808-9a40-f5608840c4bf>
2011-11-13 17:43:56 +0000

    мало поједностављено и прегледније

--- trunk/DvaDekadnaStepena.pas
+++ trunk/DvaDekadnaStepena.pas
@@ -1,22 +1,15 @@
 var
- a,b: string;
- p: char;
+  a,b: string;
+  p: char;
 begin
- readln(a);
- readln(p);
- readln(b);
- if p='+' then
-  if length(a)>length(b) then begin
-   delete(a,2,1);
-   insert('1',a,length(a)-length(b)+2)
-  end else if length(a)<length(b) then begin
-   delete(b,2,1);
-   insert('1',b,length(b)-length(a)+2);
-   a:=b
-  end else begin
-   a:='2'+copy(a,2,length(a))
-  end
- else if p='*' then insert(copy(b,2,length(b)-1),a,2);
- writeln(a)
+  readln(a);
+  readln(p);
+  readln(b);
+  if p='+' then
+    if      length(a)>length(b) then а := copy(a, 1, length(a)-length(b)) + b
+    else if length(a)<length(b) then а := copy(b, 1, length(b)-length(a)) + a
+    else                             a := '2' + copy(a, 2, length(a))
+  else if p='*' then insert(copy(b,2,length(b)-1),a,2);
+  writeln(a)
 end.
 
