64cd6eb39d63aac2f0020c29f91dd2ffc911e99e
ndjapic <ndjapic@gmail.com>
2012-12-08 17:56:59 +0000

    završen opis algoritma, treba napraviti ispravan ulaz i izlaz

--- GreedyGiftGivers.pas
+++ GreedyGiftGivers.pas
@@ -1,27 +1,42 @@
-program gift1;
- var np,i,j: integer;
- s: string:
+program gift1('gift1.in','gift1.out');
+
+ var np, i, j, a, b, q, giver, receiver: integer;
+ name: string;
  names: array [1..10] of string;
  balances: array [1..10] of integer;
 
- function pin(s: string): integer;
+ function pin(name: string): integer;
  var i: integer;
  begin
-  i:=0;
+  i := 0;
   repeat
-   i:=i+1;
-  until names[i]=s;
-  pin:=i
+   i := i+1
+  until names[i]=name;
+  pin := i
  end;
  
 begin
  readln(np);
- for i:=1 to np do begin
+ for i := 1 to np do begin
   readln(names[i]);
-  balances[i]:=0;
+  balances[i] := 0
  end;
- for i:=1 to np do begin
- readln(s);
+ for i := 1 to np do begin
+  readln(name);
+  giver := pin(name);
+  read(a); readln(b);
+  if b>0 then begin
+   q := a div b;
+   balances[giver] := balances[giver] - b*q;
+   for j := 1 to b do begin
+    readln(name);
+    receiver := pin(name);
+    balances[receiver] := balances[receiver] + q
+   end
+  end
  end;
- 
+ for i := 1 to np do begin
+  write(names[i]);
+  writeln(balances[i])
+ end
 end.
