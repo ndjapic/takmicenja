a06f3890b141b20a5db152f607d753cf2612c1d6
ndjapic@gmail.com <ndjapic@gmail.com@266d48c1-7390-4808-9a40-f5608840c4bf>
2013-01-23 12:37:19 +0000

    napravljeno nekoliko funkcija

--- trunk/ogrlica.pas
+++ trunk/ogrlica.pas
@@ -1,11 +1,54 @@
-program ogrlica;
+program beads;
 var
- ch1,ch2: char;
- n,w1,w2,w3,b,r: integer;
+ arr: array[ 1..100 ]of char;
+ ch: char;
+ d, i, n, w1, w2, w3, b, r, max: integer;
+
+function rastojanjeDoObojenePerle(): integer;
+var i: integer;
 begin
- readln(n);
+ i := 0;
  repeat
-  read(ch1);
- until not ch1='w';
- while  
+  read(f, ch);
+  i := i+1
+ until not ch='w';
+ rastojanjeDoObojenePerle := i
+end;
+
+function brojBelihIspredDrugeBoje(ch2: char): integer;
+var d2: integer;
+begin
+ while ch=ch2 do d2 := rastojanjeDoObojenePerle();
+ brojBelihIspredDrugeBoje := d2-1
+end;
+
+function rastojanjeDoDrugeBoje(ch3: char): integer;
+var d3: integer;
+begin
+ d3 := 0;
+ while ch=ch3 do d3 := d3 + rastojanjeDoObojenePerle();
+ rastojanjeDoDrugeBoje := d3
+end;
+
+begin
+ assign(f, 'beads.in');
+ reset(f);
+ readln(f, n);
+ for i := 1 to n do begin
+  read(f, arr[i]);
+  arr[n+i] := arr[i];
+ end;
+ close(f);
+ reset(f);
+ max := 0;
+ d := rastojanjeDoObojenePerle();
+ w1 := brojBelihIspredDrugeBoje(ch);
+ b := rastojanjeDoDrugeBoje(ch);
+ r := rastojanjeDoDrugeBoje(ch);
+...
+ close(f);
+ assign(g, 'beads.out');
+ rewrite(g);
+ writeln(max);
+ close(g);
 end.
