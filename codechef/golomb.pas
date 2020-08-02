10132a79f1441a160d1b6809ba89f2c3c4d8740b
Nenad Đapić <ndjapic@users.noreply.github.com>
2020-08-02 16:54:20 +0200

    Create golomb.pas

---
+++ codechef/golomb.pas
@@ -0,0 +1,112 @@
+program golomb;
+const
+    ten5 = 1000 * 100;
+    ten9 = 1000 * 1000 * 1000;
+    ten10 = sqr(ten5);
+    bigprime = ten9 + 7;
+    igsz = 100 * 100;
+    debug = true;
+var
+    tcases, n, i, l, r, iglen: integer;
+    g, ig, sg, s2g: array [1 .. igsz] of integer;
+
+function sumsqr(n: integer): integer;
+var
+    triangular, oddnum: integer;
+begin
+    triangular := n * (n + 1) div 2;
+    if triangular mod 3 = 0 then begin
+        triangular := triangular div 3;
+        oddnum := 2 * n + 1;
+    end else
+        oddnum := (2 * n + 1) div 3;
+    sumsqr := triangular mod bigprime * oddnum mod bigprime;
+end;
+
+function iginv(n: integer): integer;
+var
+    k, lo, hi: integer;
+begin
+    lo := 1;
+    hi := iglen;
+    while lo < hi do begin
+        k := (lo + hi) div 2;
+        if n > ig[k] then
+            lo := k + 1
+        else
+            hi := k;
+    end;
+    iginv := lo;
+end;
+
+function gfun(n: integer): integer;
+var
+    k: integer;
+begin
+    k := iginv(n);
+    gfun := sg[k] - (ig[k] - n) div k;
+end;
+
+function s2gfun(n: integer): integer;
+var
+    k: integer;
+begin
+    if n = 0 then
+        s2gfun := 0
+    else begin
+        k := iginv(n);
+        s2gfun := (s2g[k] + bigprime - (
+            sumsqr(sg[k]) + bigprime -
+            sumsqr(sg[k] - (ig[k] - n) div k) +
+            (ig[k] - n) mod k * sqr(sg[k])
+        ) mod bigprime) mod bigprime;
+    end;
+end;
+
+procedure init();
+var
+    i: integer;
+begin
+    g[1] := 1;
+    ig[1] := 1;
+    sg[1] := 1;
+    s2g[1] := 1;
+
+    i := 1;
+    while (i < igsz) and (ig[i] < ten10) do begin
+        inc(i);
+        g[i] := 1 + g[ i - g[g[i-1]] ];
+        ig[i] := ig[i-1] + i * g[i];
+        sg[i] := sg[i-1] + g[i];
+        s2g[i] := ( s2g[i-1] + i * (
+            sumsqr(sg[i]) + bigprime - sumsqr(sg[i-1])
+        ) ) mod bigprime;
+    end;
+    iglen := i;
+
+    if debug then begin
+        dec(i); writeln('ig[ ', i, ' ] = ', ig[i]);
+        inc(i); writeln('ig[ ', i, ' ] = ', ig[i]);
+
+        for i := 1 to 9 do begin
+            write('g( ', i, ' ) = ', gfun(i));
+            writeln('  ***  g[ ', i, ' ] = ', g[i]);
+        end;
+
+        i := 1;
+        while i < ten10 do begin
+            i := i * 10;
+            writeln('g( ', i, ' ) = ', gfun(i));
+        end;
+    end;
+end;
+
+begin
+    init();
+    readln(tcases);
+    repeat
+        readln(l, r);
+        writeln(( s2gfun(r) + bigprime - s2gfun(l-1) ) mod bigprime);
+        dec(tcases);
+    until tcases = 0;
+end.
