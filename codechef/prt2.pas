ad213e2e0d6329fb8afc49e35d0d049030667c1d
Nenad Đapić <ndjapic@users.noreply.github.com>
2020-08-06 11:48:01 +0200

    Update prt2.pas

--- codechef/prt2.pas
+++ codechef/prt2.pas
@@ -1,22 +1,70 @@
 program prt2;
 const
-    vmax = 300 * 1000;
+    nodesize = 300 * 1000;
+    edgesize = 600 * 1000;
 var
-    tcases, n, k, i, r, u, v, uv: integer;
-    minbranch, minpath: integer;
-    a: array [1 .. vmax] of integer;
-    nodes: array [1 .. vmax] of record
+    tcases, n, k, i, root, u, v, uv: integer;
+    hlen, q, r, mp: integer;
+    a: array [1 .. nodesize] of integer;
+    nodes: array [1 .. nodesize] of record
         parent, first, branch, path: integer;
     end;
-    edges: array [1 .. 2 * vmax] of record
+    edges: array [1 .. edgesize] of record
         destination, next: integer;
     end;
+
+procedure swap(i, j: integer);
+var
+    tmp: integer;
+begin
+    tmp := a[i];
+    a[i] := a[j];
+    a[j] := tmp;
+end;
+
+function prioch(pa: integer): integer;
+var
+    le, ri: integer;
+begin
+    le := pa * 2;
+    ri := le + 1;
+    if (ri <= hlen) and (a[ri] > a[le]) then
+        prioch := ri
+    else
+        prioch := le;
+end;
+
+procedure siftdown(pa: integer);
+var
+    ch: integer;
+begin
+    ch := prioch(pa);
+    while (ch <= hlen) and (a[ch] > a[pa]) do begin
+        swap(pa, ch);
+        pa := ch;
+        ch := prioch(pa);
+    end;
+end;
+
+procedure heapify();
+var
+    pa: integer;
+begin
+    for pa := hlen div 2 downto 1 do
+        siftdown(pa);
+end;
+
+procedure heapop();
+begin
+    swap(1, hlen);
+    dec(hlen);
+    siftdown(1);
+end;
 
 procedure dfs(u: integer);
 var
     uv, mp, mb1, mb2: integer;
 begin
-    nodes[u].branch := 1;
     mp := n;
     mb1 := n;
     mb2 := n;
@@ -24,7 +72,6 @@ begin
 
     while uv <> 0 do begin
         v := edges[uv].destination;
-
         if v <> nodes[u].parent then begin
             nodes[v].parent := u;
 
@@ -36,11 +83,13 @@ begin
             end;
             mp := min(mp, nodes[v].path);
         end;
-
         uv := edges[uv].next;
     end;
 
-    nodes[u].branch := mb1 + 1;
+    if mb1 < n then
+        nodes[u].branch := mb1 + 1
+    else
+        nodes[u].branch := 1;
     nodes[u].path := min(mp, mb1 + 1 + mb2);
 end;
 
@@ -67,7 +116,27 @@ begin
             nodes[v].first := 2 * uv + 1;
         end;
 
-        dfs(1);
+        if n = 2 then
+            mp := 2
+        else begin
+            root := 1 + random(n);
+            uv := nodes[root].first;
+            if edges[uv].next = 0 then
+                root := edges[uv].destination;
+            dfs(root);
+            mp := nodes[root].path;
+        end;
+
+        heapify();
+        a[n+1] := 0;
+        for i := n downto n - mp + 1 do begin
+            heapop();
+            inc(a[i], a[i+1]);
+        end;
+
+        q := k div (2 * mp);
+        r := k mod (2 * mp);
+        writeln(q * a[n+1 - mp] + a[n+1 - r div 2] + a[n+1 - (r+1) div 2]);
 
         dec(tcases);
     until tcases = 0;
