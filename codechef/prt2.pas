eab6cb18240142901d71ce97ff04f7190a9b2d87
Nenad Đapić <ndjapic@users.noreply.github.com>
2020-08-07 12:25:31 +0200

    Update prt2.pas

--- codechef/prt2.pas
+++ codechef/prt2.pas
@@ -2,12 +2,13 @@ program prt2;
 const
     nodesize = 300 * 1000;
     edgesize = 600 * 1000;
+    debug = false;
 var
     tcases, n, k, i, root, u, v, uv: integer;
-    hlen, q, r, mp: integer;
-    a: array [1 .. nodesize] of integer;
+    hlen, qot, rem, path, ans: integer;
+    a: array [1 .. nodesize + 1] of integer;
     nodes: array [1 .. nodesize] of record
-        parent, first, branch, path: integer;
+        parent, first, branch, branch2, path: integer;
     end;
     edges: array [1 .. edgesize] of record
         destination, next: integer;
@@ -28,6 +29,7 @@ var
 begin
     le := pa * 2;
     ri := le + 1;
+    {if debug then writeln('at prioch ri = ', ri);}
     if (ri <= hlen) and (a[ri] > a[le]) then
         prioch := ri
     else
@@ -50,12 +52,13 @@ procedure heapify();
 var
     pa: integer;
 begin
-    for pa := hlen div 2 downto 1 do
-        siftdown(pa);
+    {if debug then writeln('at heapify hlen = ', hlen);}
+    for pa := hlen div 2 downto 1 do siftdown(pa);
 end;
 
 procedure heapop();
 begin
+    if debug then writeln('at pop hlen = ', hlen);
     swap(1, hlen);
     dec(hlen);
     siftdown(1);
@@ -63,11 +66,11 @@ end;
 
 procedure dfs(u: integer);
 var
-    uv, mp, mb1, mb2: integer;
+    uv: integer;
 begin
-    mp := n;
-    mb1 := n;
-    mb2 := n;
+    nodes[u].path := n;
+    nodes[u].branch := n;
+    nodes[u].branch2 := n;
     uv := nodes[u].first;
 
     while uv <> 0 do begin
@@ -75,25 +78,41 @@ begin
         if v <> nodes[u].parent then begin
             nodes[v].parent := u;
 
+            if debug then writeln('bef uv = ', uv);
             dfs(v); (* RECURSION *)
+            if debug then writeln('aft uv = ', uv);
 
-            if nodes[v].branch <= mb1 then begin
-                mb2 := mb1;
-                mb1 := nodes[v].branch;
+            if nodes[v].branch <= nodes[u].branch then begin
+                nodes[u].branch2 := nodes[u].branch;
+                nodes[u].branch := nodes[v].branch;
             end;
-            mp := min(mp, nodes[v].path);
+            nodes[u].path := min(nodes[u].path, nodes[v].path);
         end;
         uv := edges[uv].next;
     end;
 
-    if mb1 < n then
-        nodes[u].branch := mb1 + 1
+    nodes[u].path := min(
+        nodes[u].path,
+        nodes[u].branch + 1 +
+        nodes[u].branch2
+    );
+
+    if nodes[u].branch < n then
+        inc(nodes[u].branch)
     else
         nodes[u].branch := 1;
-    nodes[u].path := min(mp, mb1 + 1 + mb2);
+
+    if debug then
+        writeln(
+            'u = ', u,
+            ', branch = ', nodes[u].branch,
+            ', branch2 = ', nodes[u].branch2,
+            ', path = ', nodes[u].path
+        );
 end;
 
 begin
+    randomize;
     readln(tcases);
     repeat
 
@@ -116,27 +135,55 @@ begin
             nodes[v].first := 2 * uv + 1;
         end;
 
+        for u := 1 to n do begin
+            uv := nodes[u].first;
+            if debug then
+                write('u = ', u);
+            repeat
+                if debug then
+                    write(', uv = ', uv,
+                        '->', edges[uv].destination);
+                uv := edges[uv].next;
+            until uv = 0;
+            if debug then
+                writeln;
+        end;
+
         if n = 2 then
-            mp := 2
+            path := 2
         else begin
             root := 1 + random(n);
+            {if debug then
+                writeln('root = ', root);}
             uv := nodes[root].first;
             if edges[uv].next = 0 then
                 root := edges[uv].destination;
+            {if debug then
+                writeln('root = ', root);}
+            nodes[root].parent := 0;
             dfs(root);
-            mp := nodes[root].path;
+            path := nodes[root].path;
         end;
 
+        hlen := n;
         heapify();
         a[n+1] := 0;
-        for i := n downto n - mp + 1 do begin
+        for i := n downto n+1 - path do begin
             heapop();
             inc(a[i], a[i+1]);
         end;
 
-        q := k div (2 * mp);
-        r := k mod (2 * mp);
-        writeln(q * a[n+1 - mp] + a[n+1 - r div 2] + a[n+1 - (r+1) div 2]);
+        if debug then writeln('path = ', path);
+        qot := k div (2 * path);
+        rem := k mod (2 * path);
+        if rem = 2 then
+            ans := a[n+1 - 2]
+        else
+            ans := a[n+1 - (rem+1) div 2] +
+            a[n+1 - rem div 2];
+        inc(ans, 
+            a[n+1 - path] * 2 * qot);
+        writeln(ans);
 
         dec(tcases);
     until tcases = 0;
