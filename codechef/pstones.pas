6a9206d87deb515a2ddd4cfb1714793bb5ca3a95
Nenad Đapić <ndjapic@users.noreply.github.com>
2020-08-20 22:50:11 +0200

    Update pstones.pas

--- codechef/pstones.pas
+++ codechef/pstones.pas
@@ -20,6 +20,7 @@ type
     tnode = record
         leaves: mask;
         ch: array [boolean] of longint;
+        h: shortint;
     end;
     graph = record
         v: array [1 .. maxv] of record
@@ -46,7 +47,9 @@ var
     cr: crown;
     bt: record
         a: array [1 .. maxbt] of tnode;
-        len: longint;
+        len, root: longint;
+        ldir: boolean;
+        {lbf: shortint;}
     end;
     colset: array [0 .. maxv, 0 .. maxj] of char;
     lgt: array [0 .. maxlgn] of shortint;
@@ -143,38 +146,86 @@ begin
     htfound := found;
 end;
 
+procedure updateheight(bi: longint);
+var
+    cht, chf: longint;
+begin
+    cht := node.ch[true];
+    chf := node.ch[false];
+    bt.a[bi].h := 1 + max( bt.a[cht].h, bt.a[chf].h );
+end;
+
+procedure rotate(root: longint; dir: boolean);
+var
+    pivot: longint;
+begin
+    pivot := bt.a[root].
+    bt.a[root].ch[not dir] := bt.a[pivot].ch[dir];
+    bt.a[pivot].ch[dir] := root;
+    bt.root := pivot;
+    updateheight(root);
+    updateheight(pivot);
+end;
+
 function btfound(bi: longint): boolean;
 var
     cmp: int64;
     node: tnode;
-    dir: boolean;
+    dir, found: boolean;
+    bf: shortint;
+    chd, chn: longint;
 begin
     {assert(bi <= maxbt);}
-    if bi > bt.len then begin
+    found := bi <= bt.len;
+    if not found then begin
+
         inc(bt.len);
         {if bt.len >= length(bt.a) - 2 then
             setlength(bt.a, 2 * length(bt.a));}
         node.leaves := cr.leaves;
         node.ch[false] := 0;
         node.ch[true] := 0;
+        node.h := 1;
         bt.a[bi] := node;
-        btfound := false;
+
     end else begin
+
         node := bt.a[bi];
         cmp := cmpleaves(cr.leaves, node.leaves);
         {if cr.leaves[1] <> node.leaves[1] then
             cmp := cr.leaves[1] - node.leaves[1]
         else
             cmp := cr.leaves[0] - node.leaves[0];}
-        if cmp = 0 then
-            btfound := true
-        else begin
+        if cmp <> 0 then begin
+
             dir := cmp > 0;
+            chd := node.ch[dir];
+            chn := node.ch[not dir];
             if node.ch[dir] = 0 then
                 bt.a[bi].ch[dir] := bt.len + 1;
-            btfound := btfound(bt.a[bi].ch[dir]);
+
+            found := btfound(bt.a[bi].ch[dir]);
+
+            bt.a[bi].ch[dir] := bt.root;
+            chd := bt.a[bi].ch[dir];
+            chn := bt.a[bi].ch[not dir];
+            bf := bt.a[chd].h - bt.a[chn].h;
+            if bf > 1 then begin
+                if bt.ldir <> dir then begin
+                    rotate(chd, dir);
+                    bt.a[bi].ch[dir] := bt.root;
+                end;
+                rotate(bi, not dir);
+            end;
+
+            bt.ldir := dir;
+            {bt.lbf := bf;}
+            updateheight(bi);
+
         end;
     end;
+
+    btfound := found;
 end;
 
 {procedure dfs(i: shortint; base: crown);}
@@ -185,7 +236,7 @@ var
     m: mask;
 begin
     {assert(i >= 0);}
-    if {(i >= 0) and} not {btfound(1)} htfound(cr.leaves) then begin
+    if {(i >= 0) and} not {btfound(bt.root)} htfound(cr.leaves) then begin
         colset[i, cr.j] := '1';
         m := cr.leaves;
         for di := 0 to 1 do begin
@@ -305,6 +356,8 @@ begin
                 colset[i, j1] := '0';
 
         bt.len := 0;
+        bt.a[0].h := 0;
+        bt.root := 1;
         cr.j := 0;
 
         {dfs(n, cr);}
