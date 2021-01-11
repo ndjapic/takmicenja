97669b13d0225aaae7ecd1016b6b2a703c60390c
Nenad Đapić <ndjapic@users.noreply.github.com>
2021-01-11 23:10:54 +0100

    Update avl.pas

--- sort/avl.pas
+++ sort/avl.pas
@@ -7,23 +7,34 @@ type
         root, size: int32;
         nodes: array [0 .. maxt] of record
             data: tdata;
-            freq: int32;
+            freq, parent: int32;
             childs: array [0 .. 1] of int32;
-            height: int8;
+            height, direction: int8;
         end;
     end;
 var
-    t, i: int32;
+    t, i, j, u, v, freq: int32;
     a: array [1 .. maxt] of tdata;
     tree: tavltree;
 
 procedure createtree;
 begin
+    tree.root := 0;
     tree.size := 0;
-    tree.nodes[tree.size].height := 0;
+    tree.nodes[0].height := 0;
 end;
 
-procedure appendnode(data: tdata);
+procedure connect(parent, child: int32; direction: int8);
+begin
+    if parent = 0 then
+        tree.root := child
+    else
+        tree.nodes[parent].childs[direction] := child;
+    tree.nodes[child].parent := parent;
+    tree.nodes[child].direction := direction;
+end;
+
+procedure appendnode(data: tdata; parent: int32; direction: int8);
 begin
     inc(tree.size);
     tree.nodes[tree.size].data := data;
@@ -31,42 +42,106 @@ begin
     tree.nodes[tree.size].childs[0] := 0;
     tree.nodes[tree.size].childs[1] := 0;
     tree.nodes[tree.size].height := 1;
+    connect(parent, tree.size, direction);
 end;
 
-function rotate(root: int32; direction: int8): int32;
+procedure rotate(pivot: int32);
 var
-    pivot: int32;
+    root, child, parent: int32;
+    direction: int8;
 begin
-    pivot := tree.nodes[root].childs[1 - direction];
-    tree.nodes[root].childs[1 - direction] :=
-        tree.nodes[pivot].childs[direction];
-    tree.nodes[pivot].childs[direction] := root;
-    rotate := pivot;
+    assert((pivot <> 0) and (pivot <> tree.root));
+    root := tree.nodes[pivot].parent;
+    parent := tree.nodes[root].parent;
+    direction := 1 - tree.nodes[pivot].direction;
+    child := tree.nodes[pivot].childs[direction];
+    connect(root, child, 1 - direction);
+    connect(parent, pivot, tree.nodes[root].direction);
+    connect(pivot, root, direction);
 end;
 
-procedure searchappend(data: tdata; root, parent: int32; direction: int8);
+procedure searchappend(data: tdata; root: int32);
 var
-    dif: int32;
+    dif, child, parent, sibling: int32;
+    direction, dir2: int8;
 begin
-    if root = 0 then begin
-        appendnode(data);
-        if parent = 0 then
-            tree.root := tree.size
-        else
-            tree.nodes[parent].childs[direction] := tree.size;
-    end else begin
+    if root = 0 then
+        appendnode(data, 0, 0)
+    else begin
         dif := data - tree.nodes[root].data;
         if dif = 0 then
             inc(tree.nodes[root].freq)
         else begin
             direction := ord(dif > 0);
-            searchappend(data, tree.nodes[root].childs[direction], root, direction);
+            child := tree.nodes[root].childs[direction];
+            if child = 0 then
+                appendnode(data, root, direction)
+            else begin
+                searchappend(data, child);
+                if tree.nodes[child].height =
+                   tree.nodes[root].height then
+                    inc(tree.nodes[root].height);
+                parent := tree.nodes[root].parent;
+                if parent <> 0 then begin
+                    dir2 := tree.nodes[root].direction;
+                    sibling := tree.nodes[parent].childs[1 - dir2];
+                    if tree.nodes[root].height -
+                       tree.nodes[sibling].height >= 2 then begin
+                        if dir2 <> direction then rotate(child);
+                        rotate(root);
+                    end;
+                end;
+            end;
         end;
     end;
 end;
 
+function firstnode(root: int32): int32;
+var
+    u, v: int32;
+begin
+    u := root;
+    v := tree.nodes[u].childs[0];
+    while v <> 0 do begin
+        u := v;
+        v := tree.nodes[u].childs[0];
+    end;
+    firstnode := u;
+end;
+
+function nextnode(u: int32): int32;
+begin
+    v := tree.nodes[u].childs[1];
+    if v <> 0 then
+        u := firstnode(v)
+    else begin
+        while (u <> 0) and (tree.nodes[u].direction = 1) do
+            u := tree.nodes[u].parent;
+        if u <> 0 then
+            u := tree.nodes[u].parent;
+    end;
+    nextnode := u;
+end;
+
+
 begin
     readln(t);
     for i := 1 to t do readln(a[i]);
+
+    createtree;
+    for i := 1 to t do searchappend(a[i], tree.root);
+
+    u := firstnode(tree.root);
+    i := 0;
+    while u <> 0 do begin
+
+        freq := tree.nodes[u].freq;
+        for j := i + 1 to i + freq do
+            a[j] := tree.nodes[u].data;
+        inc(i, freq);
+        u := nextnode(u);
+
+    end;
+
     for i := 1 to t do writeln(a[i]);
 end.
