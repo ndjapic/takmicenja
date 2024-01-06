8f098e18f85be86695b38815e515a546ad9ca0f4
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-01-06 12:32:55 +0100

    Update gcd.pas

--- templates/gcd.pas
+++ templates/gcd.pas
@@ -1,4 +1,10 @@
 program gcd;
+const
+    maxn = 200 * 1000;
+var
+    ntc, tci: int32;
+    n, g, i: int32;
+    a: array [1 .. maxn] of int32;
 
 function gcd(a, b: int32): int32;
 begin
@@ -10,8 +16,23 @@ end;
 
 function lcm(a, b: int64): int64;
 begin
-    lcm := a div gcd(a, b) * b;
+    lcm := int64(a) div gcd(a, b) * b;
 end;
 
 begin
+    readln(ntc);
+    for tci := 1 to ntc do begin
+
+        readln(n);
+
+        g := 0;
+        for tci := 1 to n do begin
+            read(a[i]);
+            g := gcd(g, a[i]);
+        end;
+        readln;
+
+        writeln(g);
+
+    end;
 end.
