1dbdeb00879c8a65c6e56f9f6f93691f72418633
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-04-12 00:11:09 +0200

    Update hasht.pas

--- templates/hasht.pas
+++ templates/hasht.pas
@@ -1,4 +1,4 @@
-program C_Colorful_Beans;
+program hasht;
 uses
     math;
 const
@@ -17,38 +17,37 @@ begin
     htn := 0;
 end;
 
-procedure ht_append(x, y: int32);
+function ht_append(x, y: int32): int32;
 begin
     inc(htn);
     ht[htn].x := x xor ran;
     ht[htn].y := y;
     ht[htn].l := 0;
     ht[htn].r := 0;
+    ht_append := htn;
 end;
 
 procedure ht_update(x, y: int32);
 var
-    u, v: int32;
+    u, v, xr: int32;
 begin
-    x := x xor ran;
+    xr := x xor ran;
     v := 1;
 
-    while (v > 0) and (x <> ht[v].x) do begin
+    while (v > 0) and (xr <> ht[v].x) do begin
         u := v;
-        if x < ht[u].x then
+        if xr < ht[u].x then
             v := ht[v].l
         else
             v := ht[v].r;
     end;
 
-    if v = 0 then begin
-        ht_append(x xor ran, y);
-        if x < ht[u].x then
-            ht[u].l := htn
-        else
-            ht[u].r := htn;
-    end else {if d[v].x = c then}
-        ht[v].y := min(ht[v].y, y);
+    if v > 0 then
+        ht[v].y := min(ht[v].y, y)
+    else if xr < ht[u].x then
+        ht[u].l := ht_append(x, y)
+    else
+        ht[u].r := ht_append(x, y);
 end;
 
 begin
@@ -64,8 +63,7 @@ begin
     end;
 
     a := ht[1].y;
-    for v := 1 to htn do
-        a := max(a, ht[v].y);
+    for v := 1 to htn do a := max(a, ht[v].y);
     writeln(a);
 
 end.
