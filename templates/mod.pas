767daa0a5b2e92c0cdcd23daac3ecb3ea7d76dc6
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-06-15 20:04:12 +0200

    Update mod.pas

--- templates/mod.pas
+++ templates/mod.pas
@@ -6,19 +6,24 @@ var
     n: int32;
     fact, invf: array [0 .. maxn] of int32;
 
+function modmul(a, b: int32): int32;
+begin
+    modmul := int64(a) * b mod prime;
+end;
+
 function modpow(b, e: int32): int32;
 begin
     if e = 0 then
         modpow := 1
     else if odd(e) then
-        modpow := int64(b) * modpow(b, e-1) mod prime
+        modpow := modmul(b, modpow(b, e-1))
     else
-        modpow := modpow(int64(b) * b mod prime, e div 2);
+        modpow := modpow(modmul(b, b), e div 2);
 end;
 
 function ncr(n, r: int32): int32;
 begin
-    ncr := int64(invf[r]) * invf[n-r] mod prime * fact[n] mod prime;
+    ncr := modmul(modmul(invf[r], invf[n-r]), fact[n]);
 end;
 
 procedure modinc(var a: int32; b: int32);
@@ -35,8 +40,8 @@ end;
 
 begin
     fact[0] := 1;
-    for n := 1 to maxn do fact[n] := int64(n) * fact[n-1] mod prime;
+    for n := 1 to maxn do fact[n] := modmul(n, fact[n-1]);
     invf[maxn] := modpow(fact[maxn], prime - 2);
-    for n := maxn downto 1 do invf[n-1] := int64(n) * invf[n] mod prime;
+    for n := maxn downto 1 do invf[n-1] := modmul(n, invf[n]);
 
 end.
