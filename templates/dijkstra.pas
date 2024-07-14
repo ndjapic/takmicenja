fb922d876319c5c3cc9c2dd12373ea4c940581f1
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-07-14 19:59:42 +0200

    Update dijkstra.pas

--- templates/dijkstra.pas
+++ templates/dijkstra.pas
@@ -24,72 +24,73 @@ type
         procedure sink(u: int32; x: T);
         procedure dequeue(u: int32);
     end;
-    TPrioQueue32 = specialize TPrioQueue<int32>;
+    {TPrioQueue32 = specialize TPrioQueue<int32>;}
 
 var
     n, m, i, u, v, s, t, k: int32;
     adj, par, wei, pos, rev: array [1 .. nn] of int32;
     d: array [1 .. nn] of int64;
     sib, tar: array [-nn .. nn] of int32;
-    pq: TPrioQueue32;
+    {pq: TPrioQueue32;}
+    pq: specialize TPrioQueue<int32>;
 
-    constructor TPrioQueue.Create();
-    begin
-        setlength(a, 1);
-        n := 0;
-    end;
+constructor TPrioQueue.Create();
+begin
+    setlength(a, 1);
+    n := 0;
+end;
 
-    function TPrioQueue.prior(l, r: T): boolean;
-    begin
-        prior := d[l] < d[r];
-    end;
+function TPrioQueue.prior(l, r: T): boolean;
+begin
+    prior := d[l] < d[r];
+end;
 
-    procedure TPrioQueue.pqset(v: int32; x: T);
-    begin
-        pq.a[v] := x;
-        pos[x] := v;
-    end;
+procedure TPrioQueue.pqset(v: int32; x: T);
+begin
+    a[v] := x;
+    pos[x] := v;
+end;
 
-    procedure TPrioQueue.swim(v: int32; x: T);
-    var
-        u: int32;
-    begin
+procedure TPrioQueue.swim(v: int32; x: T);
+var
+    u: int32;
+begin
+    u := v div 2;
+    while (v > 1) and prior(x, a[u]) do begin
+        pqset(v, a[u]);
+        v := u;
         u := v div 2;
-        while (v > 1) and prior(x, pq.a[u]) do begin
-            pqset(v, pq.a[u]);
-            v := u;
-            u := v div 2;
-        end;
-        pqset(v, x);
     end;
+    pqset(v, x);
+end;
 
-    procedure TPrioQueue.enqueue(x: T);
-    begin
-        inc(pq.n);
-    	if length(pq.a) <= pq.n then setlength(pq.a, 2 * pq.n);
-        swim(pq.n, x);
-    end;
+procedure TPrioQueue.enqueue(x: T);
+begin
+    inc(n);
+    if length(a) <= pq.n then setlength(a, 2*n);
+    swim(n, x);
+end;
 
-    procedure TPrioQueue.sink(u: int32; x: T);
-    var
-        v: int32;
-    begin
+procedure TPrioQueue.sink(u: int32; x: T);
+var
+    v: int32;
+begin
+    v := u * 2;
+    if (v+1 <= n) and prior(a[v+1], a[v]) then inc(v);
+    while (v <= n) and prior(a[v], x) do begin
+        pqset(u, a[v]);
+        u := v;
         v := u * 2;
-        if (v+1 <= pq.n) and prior(pq.a[v+1], pq.a[v]) then inc(v);
-        while (v <= pq.n) and prior(pq.a[v], x) do begin
-            pqset(u, pq.a[v]);
-            u := v;
-            v := u * 2;
-            if (v+1 <= pq.n) and prior(pq.a[v+1], pq.a[v]) then inc(v);
-        end;
-        pqset(u, x);
+        if (v+1 <= n) and prior(a[v+1], a[v]) then inc(v);
     end;
+    pqset(u, x);
+end;
 
-    procedure TPrioQueue.dequeue(u: int32);
-    begin
-        dec(pq.n);
-        sink(u, pq.a[pq.n + 1]);
-    end;
+procedure TPrioQueue.dequeue(u: int32);
+begin
+    dec(pq.n);
+    sink(u, a[pq.n + 1]);
+end;
 
 procedure addarrow(u, v, i: int32);
 begin
