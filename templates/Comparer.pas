1ceacc39ce59146fc9fb5384084dccaa788c40d0
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-07-23 11:22:30 +0200

    Update Comparer.pas

--- templates/Comparer.pas
+++ templates/Comparer.pas
@@ -2,10 +2,12 @@ program Comparer;
 {$mode objfpc}{$H+}{$J-}
 uses
     Generics.Defaults, Generics.Collections;
+const
+    nn = 100 * 1000;
 type
     intList = specialize TList<int32>;
-    intComparer = specialize TComparer<int32>;
 var
+    n, i: int32;
     a: intList;
 
 function intCompare(constref Left, Right: int32): int32;
@@ -14,14 +16,21 @@ begin
 end;
 
 begin
+    n := nn div 10;
     a := intList.Create();
-    a.Add(11);
-    a.Add(33);
-    a.Add(22);
-    a.Sort(intComparer.Construct(@intCompare));
 
-    Writeln('Count: ', a.Count);
-    Writeln(a[0]);
-    Writeln(a[1]);
-    Writeln(a[2]);
+    for i := 0 to nn do a.Add(random(nn));
+    for i := 0 to 10 do write(' ', a[i*n]); writeln;
+
+    a.Sort(specialize TComparer<int32>.Construct(@intCompare));
+    for i := 0 to 10 do write(' ', a[i*n]); writeln;
+    writeln(a.Count);
 end.
+(*
+ 54881 36678 74826 38613 39217 88120 75812 79091 36925 50366 30764
+ 0 9857 19828 29869 39999 49890 59703 69823 79858 89858 99999
+100001
+
+=====
+Used: 78 ms, 44 KB
+*)
